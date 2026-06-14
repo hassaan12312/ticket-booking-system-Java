ce4adc1648633fbff048b3a4ad89f56fe71e4bdf
Hassaan Mustafa <hassaanmustafa64@gmail.com>
2026-06-14 20:55:41 +0500

    added train data, fixed compile issues, and added some code for user booking service

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,12 +1,24 @@
 package ticket.booking.services;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.reflect.TypeResolver;
 
 import ticket.booking.entities.User;
 
 public class UserBookingService{
     
     private User user;
+    private static final String USERS_PATH = "../localDb/users.json";
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private List<User> userList;
 
-    public UserBookingService(User user1){
+    public UserBookingService(User user1) throws IOException{
         this.user = user1;
+        File users = new File(USERS_PATH);
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
 }
