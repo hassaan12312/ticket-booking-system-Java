ea6a0b51cddaf80f120a9c1d2366e200e3180af9
Hassaan Mustafa <hassaanmustafa64@gmail.com>
2026-06-14 17:47:37 +0500

    added initial db for users

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -1,5 +1,12 @@
 package ticket.booking.services;
 
+import ticket.booking.entities.User;
+
 public class UserBookingService{
     
+    private User user;
+
+    public UserBookingService(User user1){
+        this.user = user1;
+    }
 }
