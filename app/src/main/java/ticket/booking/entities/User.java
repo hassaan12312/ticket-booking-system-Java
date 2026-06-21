e257d92452a25528d7e4946af1122fb61b7b04f3
Hassaan Mustafa <hassaanmustafa64@gmail.com>
2026-06-21 11:37:59 +0500

    Added Getters and Login/Signup logic

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -8,4 +8,20 @@ public class User{
     private String password;
     private String hashedPassword;
     private List<Ticket> ticketsBooked;
+
+    public String getId(){
+        return userId;
+    }
+
+    public String getName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getHashedPassword(){
+        return hashedPassword;
+    }
 }
--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -2,22 +2,38 @@ package ticket.booking.services;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import ticket.booking.entities.User;
 
 public class UserBookingService{
     
     private User user;
     private static final String USERS_PATH = " app/src/main/java/ticket/booking/localDb/users.json";
-    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     private List<User> userList;
 
     public UserBookingService(User user1) throws IOException{
         this.user = user1;
         File users = new File(USERS_PATH);
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            return Boolean.TRUE;
+        } 
+        catch(Exception ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+
+        return foundUser.IsPresent();
     }
 }
